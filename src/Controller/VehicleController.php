a7cf4a7e58425a6510405a83ba42d17025c9be59
Branimir97 <branimirb51@gmail.com>
2020-12-16 22:06:49 +0100

    Done adding vehicles in database without covers/images

--- src/Controller/VehicleController.php
+++ src/Controller/VehicleController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Vehicle;
 use App\Repository\VehicleRepository;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,5 +24,33 @@ class VehicleController extends AbstractController
         $response = new JsonResponse($vehicles, 200);
 
         return $response;
+    }
+
+    /**
+     * @Route("/new")
+     * @param Request $request
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function create(Request $request) {
+        $mark = $request->query->get('mark');
+        $model = $request->query->get('model');
+        $modelYear = new \DateTime($request->query->get('modelYear'));
+        $manufactureYear = new \DateTime($request->query->get('manufactureYear'));
+        $gears = $request->query->get('gears');
+        $color = $request->query->get('color');
+        $gearbox = $request->query->get('gearbox');
+        $power = $request->query->get('power');
+        $type = $request->query->get('type');
+        $status = $request->query->get('status');
+        $price = $request->query->get('price');
+
+        $vehicle = new Vehicle();
+        $vehicle->constructObject($mark, $model ,$modelYear, $manufactureYear, $gears, $color, $gearbox, $power, $type, $status, $price);
+        $entityManager= $this->getDoctrine()->getManager();
+        $entityManager->persist($vehicle);
+        $entityManager->flush();
+
+        return new JsonResponse('success', 200);
     }
 }
